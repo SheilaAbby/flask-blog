8068bd68cafbcfd09acb58e695141b7299b438df
SheilaAbby <sheilaabigael2@gmail.com>
2018-12-29 16:49:55 +0300

    Registration,Login working

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -2,6 +2,11 @@ from flask import Flask
 
 from flask_sqlalchemy import SQLAlchemy
 
+from flask_bcrypt import Bcrypt
+
+from flask_login import LoginManager
+
+
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '3638836a16e1e56ee58871cfaf32e290'
@@ -12,6 +17,8 @@ app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
 
 # instatiate an object for SQLAlchemy module .
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 #import the routes
 from flaskblog import routes
