fdd2adacf0ca0f3b9b1e7f22a17049d3b24e70a5
SheilaAbby <sheilaabigael2@gmail.com>
2018-12-30 03:55:14 +0300

    User can update their account info

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,7 +1,9 @@
 from flask_wtf import FlaskForm 
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskblog.models import User
+
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', 
@@ -27,10 +29,33 @@ class RegistrationForm(FlaskForm):
         if user:
             raise ValidationError('That Email is taken.Please choose a different one')
 
+
 class LoginForm(FlaskForm):
     email = StringField('Email', 
                             validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username', 
+                            validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', 
+                            validators=[DataRequired(), Email()])
+    submit = SubmitField('Update Account')
+
+    #  check whether the entered username matches any in the database
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+               raise ValidationError('That username is taken.Please choose a different one')
+
+#  check whether the entered email matches any in the database
+    def validate_email(self, email):
+     if email.data != current_user.email:
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('That Email is taken.Please choose a different one')
 
