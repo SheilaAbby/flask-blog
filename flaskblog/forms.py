01af9f470bb8a086f687b16bdaca16ea39e5c3cb
SheilaAbby <sheilaabigael2@gmail.com>
2018-12-30 21:21:14 +0300

    Uplaoding and changing the profile picture

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,4 +1,5 @@
 from flask_wtf import FlaskForm 
+from flask_wtf.file import FileField, FileAllowed 
 from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
@@ -43,6 +44,7 @@ class UpdateAccountForm(FlaskForm):
                             validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email', 
                             validators=[DataRequired(), Email()])
+    picture = FileField('Update Profile picture', validators=[FileAllowed(['jpg', 'png'])])
     submit = SubmitField('Update Account')
 
     #  check whether the entered username matches any in the database
