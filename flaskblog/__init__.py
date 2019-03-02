ff94824dae0234a9b96bd0d0cf3ece1254ab0e6e
SheilaAbby <sheilaabigael2@gmail.com>
2019-03-02 12:19:35 +0300

    added custom error pages

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -10,7 +10,6 @@ from flask_mail import Mail
 
 from flaskblog.config import Config
 
-
 # instatiate an object for SQLAlchemy module .
 db = SQLAlchemy()
 bcrypt = Bcrypt()
@@ -33,11 +32,16 @@ def create_app(config_class=Config):
     from flaskblog.users.routes import users
     from flaskblog.posts.routes import posts
     from flaskblog.main.routes import main
+
+    from flaskblog.errors.handlers import errors
+    
     app.register_blueprint(users)
     app.register_blueprint(posts)
     app.register_blueprint(main)
+    app.register_blueprint(errors)
 
     return app
+
 
 
 
